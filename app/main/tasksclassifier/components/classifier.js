43b92da4cce11b7a9c58fe55fa1994021c1329a5
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-03-17 17:14:54 +0300

    test branch

--- app/main/tasksclassifier/components/classifier.js
+++ app/main/tasksclassifier/components/classifier.js
@@ -11,16 +11,16 @@ import { GridActionsCellItem } from "@mui/x-data-grid";
 import { updateMultipleDocInCollectionById } from "../../../datamodel";
 
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export const Classifier = () => {
   const router = useRouter();
@@ -29,9 +29,14 @@ export const Classifier = () => {
     router.push(`/main/tasksclassifier/${id}`);
   };
 
-  const taskscolumns = [
+  const taskscolumns2 = [
     { field: "id", headerName: "id", width: 270 },
-    { field: "description", headerName: "Название", editable: true, width: 130 },
+    {
+      field: "description",
+      headerName: "Название",
+      editable: true,
+      width: 130,
+    },
     { field: "generator", headerName: "Генератор", editable: true, width: 130 },
     { field: "function", headerName: "Функция", editable: true, width: 130 },
     { field: "variants", headerName: "Варианты", editable: true, width: 130 },
@@ -66,49 +71,48 @@ export const Classifier = () => {
     <Box sx={{ minHeight: 110, flexGrow: 1 }}>
       {/* <HorizontalLinearStepper /> */}
       <div>
-
-      {/* <textarea
+        {/* <textarea
         className="w-full h-60"
         name="postContent"
         rows={4}
         cols={40}
         defaultValue={taskProfile != undefined && selectedTasks}
       /> */}
-     
-      <div className="flex-row flex">
-        <Tree setSelected={setSelectedFolder} />
-        <Datagrid
-          collection={collection}
-          keyfield="name"
-          columns={taskscolumns}
-          checkduplic={false}
-          showhidetool={{
-            delete: "none",
-            copy: true,
-            edittask: true,
-            csvload: "none",
-            add: "none",
-            move: true,
-          }}
-          dependentFilter={selectedFolder}
-          setFilters={setSelectedTasks}
-          actions={{ movetask: handleModalOpen}}
-        />
 
-        <Modal
-          open={openMoveModal}
-          onClose={() => setOpenMoveModal(false)}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Выберите новое местоположение
-            </Typography>
-            <Tree setSelected={moveTasks} />
-          </Box>
-        </Modal>
-      </div>
+        <div className="flex-row flex">
+          <Tree setSelected={setSelectedFolder} />
+          <Datagrid
+            collection={collection}
+            keyfield="name"
+            columns={taskscolumns2}
+            checkduplic={false}
+            showhidetool={{
+              delete: "none",
+              copy: true,
+              edittask: true,
+              csvload: "none",
+              add: "none",
+              move: true,
+            }}
+            dependentFilter={selectedFolder}
+            setFilters={setSelectedTasks}
+            actions={{ movetask: handleModalOpen }}
+          />
+
+          <Modal
+            open={openMoveModal}
+            onClose={() => setOpenMoveModal(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Выберите новое местоположение
+              </Typography>
+              <Tree setSelected={moveTasks} />
+            </Box>
+          </Modal>
+        </div>
       </div>
     </Box>
   );
